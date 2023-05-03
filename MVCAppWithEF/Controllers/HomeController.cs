7366894ae601521db29b81a083c05f59f4c395ce
dan.hagen <dan.hagen@centralia.edu>
2023-05-03 08:08:31 -0700

    Add project files.

---
+++ MVCAppWithEF/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DBProject.Context;
+using DBProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using MVCAppWithEF.Models;
+using System.Diagnostics;
+
+namespace MVCAppWithEF.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        private GSContext _gsContext;
+        public HomeController(GSContext context)
+        {
+            _gsContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Customer> Cust = _gsContext.Customers.ToList();
+            return View(Cust);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCAppWithEF/Program.cs
@@ -0,0 +1,38 @@
+using DBProject.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCAppWithEF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            var conn = builder.Configuration.GetConnectionString("GourmetWin");
+            builder.Services.AddDbContext<GSContext>(
+                options => options.UseSqlServer(conn));
+          
+               var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
